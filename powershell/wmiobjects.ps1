d62a0533a84bca750dddcd8e0176b877fab0590a
daniel-ddl <59618208+daniel-ddl@users.noreply.github.com>
2020-04-07 11:02:10 -0400

    Lab 4
    wmiobjects

---
+++ powershell/wmiobjects.ps1
@@ -0,0 +1,43 @@
+﻿ echo ""
+ echo "==============="
+ echo "Computer System"
+ echo "==============="
+  get-wmiobject win32_computersystem
+ echo ""
+ echo "================"
+ echo "Operating System"
+ echo "================"
+  get-wmiobject win32_operatingsystem | format-list SystemDirectory,Version
+ echo ""
+ echo "========="
+ echo "Processor"
+ echo "========="
+  get-wmiobject win32_processor | format-list Description,Name,NumberofCores,L3CacheSize
+ echo ""
+ echo "==============="
+ echo "Physical Memory"
+ echo "==============="
+  get-wmiobject Win32_physicalmemory | format-table Manufacturer,Description,
+  @{n="Capacity(GB)"; e={$_.capacity/1gb -as [int]}},
+  BankLabel,DeviceLocator
+  get-wmiobject Win32_physicalmemory | format-list @{n="Capacity(GB)"; e={$_.capacity/1gb -as [int]}}
+echo ""
+echo "=========="
+echo "Disk Drive"
+echo "=========="
+ get-wmiobject win32_diskdrive |
+ format-table -autosize Manufacturer,Model,
+ @{n="Size(GB)"; e={$_.size/1gb -as [int]}}
+echo ""
+echo "============"
+echo "Logical Disk"
+echo "============"
+ get-wmiobject win32_logicaldisk | where-object size -gt 0 | format-table Description,
+ @{n="Size(GB)"; e={$_.size/1gb -as [int]}},
+ @{n="Free(GB)"; e={$_.freespace/1gb -as [int]}},
+ @{n="% Free"; e={100*$_.freespace/$_.size -as [int]}}
+echo ""
+echo "================"
+echo "Video Controller"
+echo "================"
+ get-wmiobject win32_videocontroller | format-list Infsection,Description,VideoModeDescription

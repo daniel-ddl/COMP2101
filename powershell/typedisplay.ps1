30500c68a5183e2dbfe3923ab9049e7c593aa473
daniel-ddl <59618208+daniel-ddl@users.noreply.github.com>
2020-04-14 11:09:45 -0400

    Lab 5
    type display and systeminfo

---
+++ powershell/typedisplay.ps1
@@ -0,0 +1,8 @@
+﻿function myfunc {
+    param ( [parameter(valuefrompipelinebypropertyname=$true)]$path )
+
+    write-output "The path variable contains '$path'"
+ 
+}
+
+new-object -typename psobject -property @{path="directory13"} | myfunc

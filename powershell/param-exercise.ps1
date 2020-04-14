f46b136271af455c59783d003af16be900d6cab2
daniel-ddl <59618208+daniel-ddl@users.noreply.github.com>
2020-04-14 11:10:06 -0400

    Lab 5 excercise

---
+++ powershell/param-exercise.ps1
@@ -0,0 +1,8 @@
+﻿Param ([Parameter(Mandatory=$true,position=1)][string]$SourceFile,
+ [Parameter(Mandatory=$true,position=2)][string]$DestinationFile )
+"SourceFile was '$sourcefile'"
+$objtypename = $sourcefile.gettype().name
+"SourceFile was a $objtypename object"
+"DestinationFile was '$destinationfile'"
+$objtypename = $destinationfile.gettype().name
+"DestinationFile was a $objtypename object"
